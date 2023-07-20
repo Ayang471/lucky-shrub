118b5953044bdba9512e42cdd767573f5123b21c
github-aliceyang <aliyang47@gmail.com>
2023-07-20 12:39:11 -0700

    created plants section component

---
+++ src/PlantsSection.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { plantList } from './PlantList'
+import './PlantsSection.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+function PlantsSection() {
+    return (
+        <div className='plants-section'>
+            <h1>FIND PLANTS YOU'LL LOVE</h1>
+            <div className='row' >
+                {plantList.map((products, index) => (
+                    <div className='col' key={index}>
+                       <div>
+                            <img src={products.image} alt='product' />
+                       </div>
+                    <div>
+                        <p>{products.title}
+                            <FontAwesomeIcon icon={faArrowRight} className='arrowRight' />
+                         </p>
+                    </div>
+                    </div>
+                ))}
+            </div>
+            <button className='ViewAllBtn'>View All Products</button>
+        </div>
+    )
+}
+
+export default PlantsSection
