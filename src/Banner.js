8e35a55756e79be21cf5a0d23a9bab5c59345b05
github-aliceyang <aliyang47@gmail.com>
2023-07-20 12:37:35 -0700

    created the banner component

---
+++ src/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className='Banner'>
+                <h1>
+                    <i>Our Mission</i>
+                    <span>is to transform any splace into an oasis you can be proud of.</span>
+                </h1>
+        </div>
+    )
+}
+
+export default Banner
