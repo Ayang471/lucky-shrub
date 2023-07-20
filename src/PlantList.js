6313143dd767987068318e76eb959efe1db12f51
github-aliceyang <aliyang47@gmail.com>
2023-07-20 12:38:55 -0700

    created the plant list array file

---
+++ src/PlantList.js
@@ -0,0 +1,34 @@
+export const plantList = [
+    {
+        title:'House Plant',
+        image:require(`./assets/houseplant.png`)
+    },
+     {
+        title:'Succulent',
+        image:require(`./assets/succulents.png`)
+    },
+     {
+        title:'Flowering Plants',
+        image:require(`./assets/plant3.png`)
+    },
+     {
+        title:'Sympathy Plant Gifst',
+        image:require(`./assets/plant4.png`)
+    },
+     {
+        title:'Bonsai & Bamboo',
+        image:require(`./assets/plant5.png`)
+    },
+     {
+        title:'Floor Plant',
+        image:require(`./assets/plant6.png`)
+    },
+     {
+         title:'Birthday Plant Gifts',
+        image:require(`./assets/plant8.png`)
+    },
+     {
+         title:'Pet Friendly',
+        image:require(`./assets/plant9.png`)
+    },
+]
