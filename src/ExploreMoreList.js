4f9920551f30ec479e45baff7c56413fff068ed8
github-aliceyang <aliyang47@gmail.com>
2023-07-20 12:39:30 -0700

    created the explore more section list array file

---
+++ src/ExploreMoreList.js
@@ -0,0 +1,17 @@
+export const ExploreMoreList = [
+    {
+        icon: require('./assets/garden.png'),
+        title:'Garden Design',
+        desc: "Lorem ipsum dolor sit amet consectetur.faucibus nulla morbi fermentum quis eu dui fringilla."
+    },
+    {
+        icon: require('./assets/landscape.png'),
+        title: 'Landscaping',
+        desc: "Lorem ipsum dolor sit amet consectetur.faucibus nulla morbi fermentum quis eu dui fringilla."
+    },
+     {
+        icon: require('./assets/maintenance.png'),
+        title: 'Maintenance',
+        desc: "Lorem ipsum dolor sit amet consectetur.faucibus nulla morbi fermentum quis eu dui fringilla."
+    }
+]
