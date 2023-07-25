255f12ae93e17bafb647b04035439fa6e023c00a
github-aliceyang <aliyang47@gmail.com>
2023-07-25 14:08:53 -0700

    created image list array

---
+++ src/ImageList.js
@@ -0,0 +1,35 @@
+export const list = [
+    {
+        image: require(`./assets/carousel1.png`)
+    },
+    {
+        image: require(`./assets/carousel2.png`)
+    },
+    {
+        image: require(`./assets/carousel3.png`)
+    },
+    {
+        image: require(`./assets/carousel4.png`)
+    },
+    {
+        image: require(`./assets/carousel5.png`)
+    },
+
+]
+
+{/* {
+        image: require(`./assets/carousel6.png`)
+    },
+    {
+        image: require(`./assets/carousel7.png`)
+    },
+    {
+        image: require(`./assets/carousel8.png`)
+    },
+    {
+        image: require(`./assets/carousel12.png`)
+    },
+    {
+        image: require(`./assets/carousel13.png`)
+    }
+ */}
