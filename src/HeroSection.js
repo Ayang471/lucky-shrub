7741abadc8b6b6a0d556c782425c182d71b924ce
github-aliceyang <aliyang47@gmail.com>
2023-07-25 14:08:39 -0700

    modified the hero section

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -1,29 +1,20 @@
 import React from 'react'
-import Hero from './assets/transparent-background1.png'
 import './HeroSection.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import Border from './Border';
 
 function HeroSection() {
   return (
     <div className='HeroSection'>
       <div classNamr='container'>
-        <div className='row'>
-          <div className='col texts'>
-            <h1>Lorem ipsum dolor sit amet consectetur. </h1>
+          <div className='texts'>
+            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur. Sit lacus et turpis porttitor facilisi lectus egestas nisl.
+            Lorem ipsum dolor sit amet consectetur. Sit lacus et turpis porttitor facilisi lectus egestas nisl. Elementum aliquet eu et mauris vulputate nibh. Egestas odio aliquet sit aliquet in nec auctor.
             </p>
             <button>Explore
               <FontAwesomeIcon icon={faArrowRight} className='arrowRight' />
             </button>
-          </div>
-          {/*<div className='col'>
-            <img src={Hero} alt='hero' />
-            <Border />
-          </div>
-          */}
         </div>
       </div>
     </div>
